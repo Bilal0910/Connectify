7aecb88737b52174600e211c6aff2329deee8ef8
Bilal0910 <bilalnadeem4u@gmail.com>
2025-01-09 18:43:43 +0500

    1st commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <p>fdfdf</p>
+
+      {/* Header */}
+
+      {/* App Body */}
+        {/* SideBar */}
+        {/* Feed */}
+        {/* Widgets */}
+        
+    </div>
+  );
+}
+
+export default App;
