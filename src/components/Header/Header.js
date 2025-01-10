59f90438b16e73344f196c9072c9882d892ec1c0
Bilal0910 <bilalnadeem4u@gmail.com>
2025-01-10 20:07:20 +0500

    3rd commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import logo1 from "../../assets/image.png";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleAltSharpIcon from '@mui/icons-material/PeopleAltSharp';
+import WorkIcon from '@mui/icons-material/Work';
+import MessageIcon from '@mui/icons-material/Message';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import HeaderOption from "../HeaderOption/HeaderOption";
+import avatar from '../../assets/avatar.webp'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src={logo1} alt="logo" />
+
+        <div className="header__search">
+          {/* SearchIcon */}
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title='Home' />
+        <HeaderOption Icon={PeopleAltSharpIcon} title='My Network' />
+        <HeaderOption Icon={WorkIcon} title='Jobs' />
+        <HeaderOption Icon={MessageIcon} title='Messaging' />
+        <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+        <HeaderOption avatar={avatar} title='me' />
+
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/HeaderOption/HeaderOption.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./HeaderOption.css"
+import { Avatar } from '@mui/material'
+
+const HeaderOption = ({Icon, title, avatar}) => {
+  return (
+    <div className='headerOption'>
+        {Icon && <Icon className="headerOption__icon" />}
+        {avatar && <Avatar className='headerOption__icon' style={{border: "0.2px solid rgb(126, 126, 126)"}} src={avatar}/>}
+        <h4 className='headerOption__title'>{title}</h4>
+      
+    </div>
+  )
+}
+
+export default HeaderOption
